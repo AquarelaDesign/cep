890b8d9d4c992d95ca474acd02ff5c4dcd7055ed
José Augusto Freire <aquarela.design@gmail.com>
2020-03-16 18:12:54 -0300

    Tratamentos para HTTPS

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,3 +1,51 @@
+const port = 3003
+const port_ssl = 3004
+
+const bodyParser = require('body-parser')
+const allowCors = require('./cors')
+
+const fs = require('fs')
+const http = require('http')
+const https = require('https')
+const express = require('express')
+const mongoose = require('mongoose')
+const server = express()
+
+mongoose.connect('mongodb://procyon:p40c10n@168.194.69.79:27017/cep', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+})
+
+// Certificate
+const Certs = JSON.parse(fs.readFileSync(__dirname+'/ssl-dev.json', 'utf8'))
+const privateKey = fs.readFileSync(Certs.privateKey, 'utf8')
+const certificate = fs.readFileSync(Certs.certificate, 'utf8')
+const ca = fs.readFileSync(Certs.ca, 'utf8')
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+}
+
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
+server.use(allowCors)
+
+const httpServer = http.createServer(server)
+const httpsServer = https.createServer(credentials, server)
+
+httpServer.listen(port, () => {
+    console.log(`BACKEND HTTP sendo executado na porta ${port}.`)
+})
+
+httpsServer.listen(port_ssl, () => {
+	console.log(`BACKEND HTTPS sendo executado na porta ${port_ssl}.`)
+})
+
+module.exports = server
+
+/*
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
@@ -18,3 +66,4 @@ server.listen(3003, function() {
 })
 
 module.exports = app
+*/
